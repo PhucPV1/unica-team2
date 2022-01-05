b65b70dd49f02dda09b83f5328ffba57d3c61c9c
PhucPV1 <phucv172@gmail.com>
2022-01-05 17:53:16 +0700

    restructure folder

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,6 @@
-const coursesRouter = require("./coursesRoutes")
 const siteRouter = require("./siteRoutes")
 
 function route(app) {
-  app.use("/courses", coursesRouter)
   app.use("/", siteRouter)
 }
 
