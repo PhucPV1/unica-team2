1736345396267f613175d1dcf1a02502b23820b0
ngomy2001 <ngomy2001@gmail.com>
2022-01-17 15:38:44 +0700

    update-crud

--- app/routes/traineeRoutes.js
+++ app/routes/traineeRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const traineeController = require('../controllers/TraineeController');
 
-router.get('/:id/updateProfile', traineeController.getUpdateProfileView);
-router.patch('/:id/updateProfile', traineeController.updateProfile);
-router.get('/:id/createTrainer', traineeController.getCreateTrainerView);
-router.patch('/:id/createTrainer', traineeController.createTrainer);
+router.get('/updateProfile', traineeController.getUpdateProfileView);
+router.patch('/updateProfile', traineeController.updateProfile);
 // router.get('/', traineeController.getUpdateProfileView);
 module.exports = router;
